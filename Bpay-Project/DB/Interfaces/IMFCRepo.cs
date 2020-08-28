34493e9ad3e3fc3687fee57cd02a6ef4784ad12c
Developer Laptop <Developer Laptop@DESKTOP-HV5I4VI>
2020-08-28 12:46:36 +0800

    Updated

---
+++ Bpay-Project/DB/Interfaces/IMFCRepo.cs
@@ -0,0 +1,16 @@
+﻿using Bpay_Project.DB.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bpay_Project.DB.Interfaces
+{
+    public interface IMFCRepo
+    {
+        IEnumerable<MFC> All { get; }
+        MFC Find(int id);
+        MFC Add(MFC model);
+        MFC Update(MFC model);
+    }
+}
---
+++ Bpay-Project/DB/Repos/MFCRepo.cs
@@ -0,0 +1,46 @@
+﻿using Bpay_Project.DB.Entities;
+using Bpay_Project.DB.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bpay_Project.DB.Repos
+{
+    public class MFCRepo : IMFCRepo
+    {
+        private readonly AppDbContext context;
+
+        public MFCRepo(AppDbContext context)
+        {
+            this.context = context;
+        }
+
+        public IEnumerable<MFC> All => context.MFC.Include(x => x.User);
+
+        public MFC Add(MFC model)
+        {
+            if(model != null)
+            {
+                context.MFC.Add(model);
+                context.SaveChanges();
+            }
+            return model;
+        }
+
+        public MFC Find(int id)
+        {
+            return context.MFC.Include(x => x.User)
+                .FirstOrDefault(x => x.Id == id);
+        }
+
+        public MFC Update(MFC model)
+        {
+            var attach = context.MFC.Attach(model);
+            attach.State = EntityState.Modified;
+            context.SaveChanges();
+            return model;
+        }
+    }
+}
